d79321eeee35bb81932e682e7e8da0616ff43b57
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-03-30 13:58:35 -0700

    add method to create a new board of tiles

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,7 +8,15 @@ import { PlayerComponent } from '../player/player.component';
 })
 export class BoardComponent {
 
-  constructor(public player: PlayerComponent) { }
+  tiles: Object[];
 
+  create() {
+    for(let i = 0; i < 10; i++) {
+      this.tiles[i] = []; //tiles is an array containing 10 arrays (rows)
+      for(let j = 0; j < 10; j++) { //each row contains 10 tile units (1 per column)
+        this.tiles[i][j] = {used: false}; //each tile is represented by [row][col] coordinates. each contains a property to track state
+      }
+    }
+  }
 
 }
